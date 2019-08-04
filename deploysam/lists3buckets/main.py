70c93384697cf59fba885ed098950c45da79ac19
Prashant <strike3test@hotmail.com>
2019-08-04 18:38:14 +1200

    second commit

--- deploysam/lists3buckets/main.py
+++ deploysam/lists3buckets/main.py
@@ -6,10 +6,22 @@ s3 = boto3.client('s3')
 def lambda_handler(event, context):
     response = s3.list_buckets()
     for bucket in response['Buckets']:
-        #print(bucket["Name"])
+        # print(bucket["Name"])
         return {
         "statuscode": 200,
         "body": json.dumps(
-            {"message": bucket["Name"]}
+            {"message": bucket['Name']}
         )
     }
+
+# ec2client = boto3.client('ec2')
+
+# def lambda_handler(event, context):
+#     regionRes = ec2client.describe_regions()
+#     return {
+#         "statuscode": 200,
+#         "body": json.dumps(
+#             {"message": regionRes['Regions']}
+#         )
+#     }
+
